2cc0dc5551be8b631c3350592843e4900d620395
Kevin Vinck <kevin.vinck@paramount.com>
2024-05-04 19:30:17 +0000

    Add copilot

--- init.lua
+++ init.lua
@@ -290,6 +290,7 @@ require('lazy').setup({
     },
     version = '^1.0.0', -- optional: only update when a new 1.x version is released
   },
+  'github/copilot.vim',
   -- NOTE: Plugins can also be configured to run Lua code when they are loaded.
   --
   -- This is often very useful to both group configuration, as well as handle
