78f8e054255e6a7f471edac27d307e284a204e4b
kvinck <kevin.vinck@paramount.com>
2024-05-26 13:45:57 -0400

    Move everything into submodules.

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -93,5 +93,52 @@ return {
       --   detached = vim.fn.has 'win32' == 0,
       -- },
     }
+
+    require('dap-python').setup()
+      dap.configurations.python = {
+        {
+          type = 'python',
+          request = 'launch',
+          name = 'Launch file',
+          program = '${file}',
+          pythonPath = path,
+        },
+        {
+          type = 'debugpy',
+          request = 'launch',
+          name = 'Django',
+          program = '${workspaceFolder}/manage.py',
+          args = {
+            'runserver',
+            '0.0.0.0:8000',
+          },
+          justMyCode = true,
+          django = true,
+          console = 'integratedTerminal',
+        },
+        {
+          type = 'python',
+          request = 'attach',
+          name = 'Attach remote',
+          connect = function()
+            return {
+              host = 'localhost',
+              port = 5678,
+            }
+          end,
+        },
+        {
+          type = 'python',
+          request = 'launch',
+          name = 'Launch file with arguments',
+          program = '${file}',
+          args = function()
+            local args_string = vim.fn.input 'Arguments: '
+            return vim.split(args_string, ' +')
+          end,
+          console = 'integratedTerminal',
+          pythonPath = path,
+        },
+      }
   end,
 }
